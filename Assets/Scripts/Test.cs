b535a1267f09444a349e7686bbb9508d61e75dda
896451711 <896451711@qq.com>
2025-06-16 19:24:43 +0800

    GitTest_Test

--- Assets/Scripts/Test.cs
+++ Assets/Scripts/Test.cs
@@ -7,7 +7,7 @@ public class Test : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        print("测试Git");
     }
 
     // Update is called once per frame
