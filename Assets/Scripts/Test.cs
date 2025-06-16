72ca88993d608e85dcfa3e18afc217383aa3f3b4
896451711 <896451711@qq.com>
2025-06-16 16:52:47 +0800

    Test

--- Assets/Scripts/Test.cs
+++ Assets/Scripts/Test.cs
@@ -7,7 +7,7 @@ public class Test : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        print("进行一次git测试");
     }
 
     // Update is called once per frame
